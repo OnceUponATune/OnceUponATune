598aa5a977961c71cd4c3a48a355518af76a8e44
Leon Liang <llian008@fiu.edu>
2017-09-30 00:48:47 -0400

    fix

--- server/server.js
+++ server/server.js
@@ -1,17 +1,27 @@
+var config = require('../webpack.config')
 var express = require('express');
 var path = require('path');
+var webpack = require('webpack')
+var webpackDevMiddleware = require('webpack-dev-middleware')
+var webpackHotMiddleware = require('webpack-hot-middleware')
 
 var app = express();
+var port = 3000;
+
+var compiler = webpack(config);
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+app.use(webpackHotMiddleware(compiler));
 
 app.use(express.static('./dist'));
 
-app.use('/', function (req, res) {
-    res.sendFile(path.resolve('../client/index.html'));
+app.get("/", function(req, res) {
+  res.sendFile(path.resolve('client/index.html'));
 });
 
-var port = 3000;
-
 app.listen(port, function(error) {
-  if (error) throw error;
-  console.log("Express server listening on port", port);
+  if (error) {
+    console.error(error)
+  } else {
+    console.log("Express server listening on port", port);
+  }
 });
