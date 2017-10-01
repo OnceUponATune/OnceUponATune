4e0311d55eaaa479ac6566add4bc82b686435f0b
Andres Moser <amose001@fiu.edu>
2017-10-01 00:12:22 -0400

    commented out test route

--- server/server.js
+++ server/server.js
@@ -29,13 +29,18 @@ app.get("/getPrompts", function(req, res) {
   }});
 });
 
+
+//not needed with song -> story
 app.get("/songChosen", function(req, res) {
-  request('http://www.google.com', function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
-  });
-  res.send('hello world');
+  // get started example
+  // request('http://www.google.com', function (error, response, body) {
+
+
+    // console.log('error:', error); // Print the error if one occurred
+    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // console.log('body:', body); // Print the HTML for the Google homepage.
+  // });
+  //res.send('hello world');
 });
 
 app.post("/sendSong", function(req, res) {
