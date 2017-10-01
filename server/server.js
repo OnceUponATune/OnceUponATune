95b3e6281f80129c39ef00e105f9affb3fcad019
Andres Moser <amose001@fiu.edu>
2017-10-01 00:00:03 -0400

    server setup

--- server/server.js
+++ server/server.js
@@ -4,9 +4,12 @@ var path = require('path');
 var webpack = require('webpack')
 var webpackDevMiddleware = require('webpack-dev-middleware')
 var webpackHotMiddleware = require('webpack-hot-middleware')
+var request = require('request');
+var keys = require('../config.json');
 
 var app = express();
 var port = 3000;
+//Python server port = 5000
 
 var compiler = webpack(config);
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
@@ -26,8 +29,24 @@ app.get("/getPrompts", function(req, res) {
   }});
 });
 
-app.post("promptChosen", function(req, res) {
+app.get("/songChosen", function(req, res) {
+  request('http://www.google.com', function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+  });
+  res.send('hello world');
+});
 
+app.post("/sendSong", function(req, res) {
+  console.log(JSON.stringify(req.body));
+  request.post('http://localhost:5000/SingSong',
+  req.body,
+  function(error, response, body) {
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+    console.log('body:', body);
+  });
 });
 
 app.listen(port, function(error) {
