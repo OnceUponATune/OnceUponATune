8e40623933f3bdac48b32e705f973f0dc4091e64
Gavin Chin <chin@Gavins-MacBook-Pro.local>
2017-10-01 01:37:49 -0400

    Pushing a semi broken frontend

--- components/PromptList.js
+++ components/PromptList.js
@@ -1,25 +1,52 @@
 import React, {Component} from 'react';
 import ReactList from 'react-list';
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 
 class PromptList extends Component {
   constructor(props){
     super(props);
     this.state = {
-      prompts : []
+      prompts : [],
+      count: 0
     }
-
   }
     testFunc(key,index){
-      return <div key={key}>{'TEST1'}</div>;
+      return <div key={key}
+                style={{
+                  marginTop: 10,
+                  marginBottom: 10,
+                  width: 800
+                }}
+              className={"container"}>
+        <Card>
+          <CardHeader style={{textAlign: "left"}}
+            title={"Short Story"}
+            />
+        <CardText style={{fontSize: 16}}>
+            The Stories Prompts will be here.
+          </CardText>
+        </Card>
+      </div>;
     }
     render(){
       return(
         <div>
-          <div style={{overflow:'auto', maxHeight: 400}}>
+          <div style={{textAlign: "center", fontSize: "40"}}>
+            /r/WritingPrompts
+          </div>
+          <div
+            style=
+            {{
+              overflow:'auto',
+              textAlign: "center",
+              marginTop: 25
+            }} >
           <ReactList
             itemRenderer={this.testFunc.bind(this)}
-            length={25}
+            length={10}
             type='uniform'
+            style={{borderStyle: "solid"}}
             />
           </div>
         </div>
